912c9b11e1e50579fa1c1584aad099b9b3e712c9
AndrewSmith12142000 <ungee.smith@gmail.com>
2023-09-22 00:07:37 -0500

    Added Stack Constructor

--- stack.cpp
+++ stack.cpp
@@ -3,9 +3,25 @@
  * modify this file as needed to complete the assignment
  */
 
-// each cpp includes ONLY it's header directly
-#include "stack.h"
 
-/*
- * write all your stack methods here
- */
+#include "stack.h"
+#include <iostream>
+
+Stack::Stack(int size) : size(size), top(-1) {
+    if (size < 2) {
+        size = 10; 
+    }
+    stack = new Data*[size];
+}
+
+Stack::~Stack() {
+    for (int i = 0; i <= top; ++i) {
+        delete stack[i];
+    }
+    delete[] stack;
+}
+
+
+bool isEmpty() const {
+    return top == -1;
+}
