683ebf9faa77b9240313508744048dda7080a767
AndrewSmith12142000 <ungee.smith@gmail.com>
2023-09-22 00:11:26 -0500

    Added push stack

--- stack.cpp
+++ stack.cpp
@@ -21,6 +21,15 @@ Stack::~Stack() {
     delete[] stack;
 }
 
+bool Stack::push(int id, const std::string& info) {
+    bool success = false; 
+    if (id >= 0 && top < size - 1) {
+        stack[++top] = new Data{ id, info };
+        success = true; 
+    }
+    return success; 
+}
+
 
 bool isEmpty() const {
     return top == -1;
