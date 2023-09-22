aafebb96c6871cd0259759ad9cd7c5e8553b8113
AndrewSmith12142000 <ungee.smith@gmail.com>
2023-09-22 03:09:41 -0500

    Added the comment header

--- data.h
+++ data.h
@@ -1,9 +1,9 @@
 /*
- * COSC 2436
- * Stacks Assignment
- * Do not modify this comment
- * 
- * DO NOT MODIFY THIS FILE
+ Name: Andrew Smith
+ Coding 04
+ Purpose: This file shows the stuct data and the string attach to it. 
+ File: data.h
+ *
  */
 
 #ifndef STACKS_DATA_H
@@ -12,15 +12,10 @@
 #include <string>
 using std::string;
 
-/*
- * Simple data structure to simulate the idea of an ADT which contains
- * an identifier and some kind of data. It's not important how complex
- * or simple this structure is, we only want to simulate the idea of
- * a single simple structure that contains both an identifier and data.
- */
+
 struct Data {
     int id;
     string information;
 };
 
-#endif /* STACKS_DATA_H */
+#endif 
